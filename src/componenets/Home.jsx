6aaee828b238ce5b56f61551b4e7c20139fdc17f
sameer tahir <sameertahir024@gmail.com>
2024-01-24 13:17:44 +0500

    second

--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Whyus from "./Whyus";
 import Improve from "./Improve";
